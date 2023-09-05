4ca3628285dd26a6abc3293ebef29b8a2062ae56
i-norden <iansnorden@gmail.com>
2023-09-05 11:25:18 -0500

    missing incrementation of subtrail index when iterating over pointer array

--- hamt.go
+++ hamt.go
@@ -909,6 +909,7 @@ func (n *Node) ForEachTracked(ctx context.Context, trail []int, f func(k string,
 		subTrail := make([]int, l, l+1)
 		copy(subTrail, trail)
 		subTrail = append(subTrail, idx)
+		idx++
 
 		if p.isShard() {
 			chnd, err := p.loadChild(ctx, n.store, n.bitWidth, n.hash)
@@ -926,7 +927,6 @@ func (n *Node) ForEachTracked(ctx context.Context, trail []int, f func(k string,
 				}
 			}
 		}
-		idx++
 	}
 	return nil
 }
@@ -961,6 +961,7 @@ func (n *Node) ForEachTrackedWithNodeSink(ctx context.Context, trail []int, b *b
 		subTrail := make([]int, l, l+1)
 		copy(subTrail, trail)
 		subTrail = append(subTrail, idx)
+		idx++
 
 		if p.isShard() {
 			chnd, err := p.loadChild(ctx, n.store, n.bitWidth, n.hash)
@@ -978,7 +979,6 @@ func (n *Node) ForEachTrackedWithNodeSink(ctx context.Context, trail []int, b *b
 				}
 			}
 		}
-		idx++
 	}
 	return nil
 }
@@ -1063,6 +1063,7 @@ func (n *Node) walkChildrenTracked(trail []int, f func(k string, val *cbg.Deferr
 		subTrail := make([]int, l, l+1)
 		copy(subTrail, trail)
 		subTrail = append(subTrail, idx)
+		idx++
 
 		if p.isShard() {
 			if p.cache != nil {
@@ -1119,6 +1120,7 @@ func (n *Node) walkChildrenTrackedWithNodeSink(trail []int, b *bytes.Buffer, sin
 		subTrail := make([]int, l, l+1)
 		copy(subTrail, trail)
 		subTrail = append(subTrail, idx)
+		idx++
 
 		if p.isShard() {
 			if p.cache != nil {
